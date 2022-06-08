1ae04f12a0893d3c064cba43df44d99a71f1dc8c
Rodewitsch <rodewitsch@inbox.ru>
2022-06-08 20:43:44 +0300

    fix: 22-2 question

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -151,7 +151,7 @@ export default function (props: ComponentProps) {
               fontSize: Global.smallScreen ? 13 : 15,
             }}
           >
-            3.0.17
+            3.0.18
           </Text>
         </View>
       </ScrollView>
